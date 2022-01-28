9c402d2bcebd03928166b2dbce2b9f314e55055d
Alan Sanchez <realalansan@gmail.com>
2022-01-28 10:31:18 -0800

    Add invite routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,3 +1,4 @@
 export { default as userRouter } from './user.routes';
 export { default as projectRouter } from './project.routes';
 export { default as bugRouter } from './bug.routes';
+export { default as inviteRouter } from './invite.routes';
