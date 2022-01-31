2ef8df225bf85742835222b7ef69e55491dbeeaf
Alan Sanchez <realalansan@gmail.com>
2022-01-31 08:12:21 -0800

    Fix route middleware and paths

--- src/routes/invite.routes.ts
+++ src/routes/invite.routes.ts
@@ -8,16 +8,14 @@ import {
 } from './../controllers/invite.controller';
 import express from 'express';
 import auth from '../middleware/auth';
-import id from '../middleware/bodyId';
-import nameDesc from '../middleware/nameDesc';
 
 const router = express.Router();
 
-router.get('/', auth, id, invites);
-router.get('/:id', auth, invite);
-router.post('/', auth, id, nameDesc, createInvite);
-router.put('/:id', auth, id, updateInvite);
-router.post('/:id', auth, id, acceptInvite);
-router.post('/:id', auth, id, declineInvite);
+router.get('/', auth, invites);
+// router.get('/:id', auth, invite);
+router.post('/', auth, createInvite);
+// router.put('/:id', auth, id, updateInvite);
+router.post('/:id/accept', auth, acceptInvite);
+router.post('/:id/decline', auth, declineInvite);
 
 export default router;
