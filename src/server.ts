bec3c45f50970f2a0cd96d25c10b9e93af963ddd
Alan Sanchez <realalansan@gmail.com>
2022-03-03 05:03:41 -0800

    Fix server config

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,16 @@
-import { __prod__, DOMAIN, COOKIE_NAME } from './config';
 import dotenv from 'dotenv';
+
+dotenv.config();
+
+import { __prod__, DOMAIN, COOKIE_NAME, SESSION_SECRET } from './config';
 import express from 'express';
 import session from 'express-session';
+import cors from 'cors';
 
 import { createRoutes } from './utils/routing';
 import { bugRouter, inviteRouter, projectRouter, userRouter } from './routes';
 
-dotenv.config();
+console.log(__prod__, DOMAIN);
 
 const app = express();
 
@@ -15,16 +19,18 @@ const PORT = process.env.PORT || 4000;
 app.use(express.json({ limit: '30mb', strict: true }));
 app.use(express.urlencoded({ limit: '30mb', extended: true }));
 
+app.use(cors({ origin: __prod__ ? DOMAIN : undefined, credentials: true }));
+
 app.use(
   session({
     name: COOKIE_NAME,
-    secret: process.env.SECRET || '123',
+    secret: SESSION_SECRET || '123',
     resave: false,
     saveUninitialized: true,
     cookie: {
       maxAge: 1000 * 60 * 60 * 24 * 365,
       httpOnly: true,
-      sameSite: 'lax',
+      sameSite: 'none',
       secure: __prod__,
       domain: __prod__ ? DOMAIN : undefined,
     },
