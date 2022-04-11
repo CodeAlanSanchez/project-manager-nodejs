19966407c161aac173df18c739f9855ffea7ceab
Alan Sanchez <realalansan@gmail.com>
2022-04-11 13:55:11 -0700

    Add logout route

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,4 +1,12 @@
-import { user, users, login, register, me } from './../controllers/user.controller';
+import {
+  user,
+  users,
+  login,
+  register,
+  me,
+  logout,
+} from './../controllers/user.controller';
+import auth from '../middleware/auth';
 import express from 'express';
 
 const router = express.Router();
@@ -7,7 +15,8 @@ router.get('/user', users);
 router.get('/user/:id', user);
 router.post('/login', login);
 router.post('/register', register);
-router.get('/me', me)
+router.get('/me', auth, me);
+router.post('/logout', auth, logout);
 // router.put('/user', );
 // router.delete('/user', );
 
